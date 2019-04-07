86741b00614493264d6fa463c778866e7bf5b3df
tim <student@neteng>
2019-04-08 09:09:30 +0930

    There was a error with the previous commited unit1 sh file

--- unit1-solution1.sh
+++ unit1-solution1.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 mkdir -p -m 775 unit1exercise1
 mkdir -m 775 unit1exercise1/ausrabarbtete
@@ -36,41 +35,3 @@ sudo chmod -c 0 unit1exercise1/belaufung/gehaltst
 sudo chmod -c 506 unit1exercise1/belaufung/enwarfkeit
 sudo tar -cvzf unit1-solution1.tgz unit1exercise1
 
-=======
-#!/bin/bash
-mkdir -p -m 775 unit1exercise1
-mkdir -m 775 unit1exercise1/ausrabarbtete
-mkdir -m 775 unit1exercise1/ausgefahrung
-mkdir -m 775 unit1exercise1/ensinner
-mkdir -m 775 unit1exercise1/belaufung
-mkdir -m 775 unit1exercise1/gestehtete
-mkdir -m 775 unit1exercise1/einhunder
-mkdir -m 775 unit1exercise1/ausgehaltt
-mkdir -m 775 unit1exercise1/auflaufst
-mkdir -p -m 775 unit1exercise1/ausrabarbtete/enhaltheit
-mkdir -p -m 775 unit1exercise1/auflaufst/ausgefahren
-mkdir -p -m 775 unit1exercise1/ausgefahrung/gesteher
-mkdir -p -m 775 unit1exercise1/belaufung/gehaltst
-mkdir -p -m 775 unit1exercise1/belaufung/enwarfkeit
-mkdir -p -m 335 unit1exercise1/auflaufst/ausgefahren/angekaest
-mkdir -p -m 636 unit1exercise1/ausrabarbtete/enhaltheit/ausgewitztete
-mkdir -p -m 506 unit1exercise1/auflaufst/ausgefahren/aufschmeckung
-mkdir -p -m 62 unit1exercise1/auflaufst/ausgefahren/gespracht
-mkdir -p -m 764 unit1exercise1/belaufung/gehaltst/zerwarft
-mkdir -p -m 423 unit1exercise1/auflaufst/ausgefahren/angekatzeheit
-mkdir -p -m 336 unit1exercise1/belaufung/enwarfkeit/ausfahrte
-chmod -c 553 unit1exercise1/ausrabarbtete
-chmod -c 313 unit1exercise1/ausgefahrung
-chmod -c 736 unit1exercise1/ensinner
-chmod -c 056 unit1exercise1/belaufung
-chmod -c 075 unit1exercise1/gestehtete
-chmod -c 632 unit1exercise1/einhunder
-chmod -c 054 unit1exercise1/ausgehaltt
-chmod -c 441 unit1exercise1/auflaufst
-sudo chmod -c 612 unit1exercise1/auflaufst/ausgefahren
-chmod -c 255 unit1exercise1/ausrabarbtete/enhaltheit
-chmod -c 63 unit1exercise1/ausgefahrung/gesteher
-sudo chmod -c 0 unit1exercise1/belaufung/gehaltst
-sudo chmod -c 506 unit1exercise1/belaufung/enwarfkeit
-sudo tar -cvzf unit1-solution1.tgz unit1exercise1
->>>>>>> c1b1903062d6cb6df0e084154c7e1f72ba20d118

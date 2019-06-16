573ba131da1971a7b5c74bb828a860e9b797c84a
tim <student@neteng>
2019-06-17 00:33:47 +0930

    git push origin master

--- unit1-solution1.sh
+++ unit1-solution1.sh
@@ -3,43 +3,37 @@ alias m="mkdir -p -m"
 alias cs="sudo chmod -c"
 alias c="chmod -c"
 m 775 unit1exercise1
-m 775 unit1exercise1/verwitztest
-m 775 unit1exercise1/angehaltheit
-m 775 unit1exercise1/anwitzkeit
-m 775 unit1exercise1/getrauheit
-m 775 unit1exercise1/angerauchheit
-m 775 unit1exercise1/angerauchtete
-m 775 unit1exercise1/auswitztest
-m 775 unit1exercise1/ankatzeen
-
-m 775 unit1exercise1/auswitztest/aufgekatzet
-m 775 unit1exercise1/angerauchtete/aufgesprachtete
-m 775 unit1exercise1/angehaltheit/aufgestehte
-m  775 unit1exercise1/ankatzeen/zerrennen
-m 775 unit1exercise1/angehaltheit/ausgesetzer
-
-m 144 unit1exercise1/ankatzeen/zerrennen/angestehheit
-m 565 unit1exercise1/angehaltheit/ausgesetzer/ausgefahrt
-m 611 unit1exercise1/angerauchtete/aufgesprachtete/zersitzkeit
-m 311 unit1exercise1/angehaltheit/aufgestehte/angeschmeckheit
-m 312 unit1exercise1/ankatzeen/zerrennen/angegehtest
-m 313 unit1exercise1/angehaltheit/aufgestehte/behaltkeit
-m 621 unit1exercise1/angehaltheit/aufgestehte/gekaesung
-
-cs 421 unit1exercise1/auswitztest/aufgekatzet
-cs 241 unit1exercise1/angerauchtete/aufgesprachtete
-cs 330 unit1exercise1/angehaltheit/aufgestehte
-cs 175 unit1exercise1/ankatzeen/zerrennen
-cs 414 unit1exercise1/angehaltheit/ausgesetzer
-
-c 370 unit1exercise1/verwitztest
-c 054 unit1exercise1/angehaltheit
-c 034 unit1exercise1/anwitzkeit
-c 614 unit1exercise1/getrauheit
-c 040 unit1exercise1/angerauchheit
-c 372 unit1exercise1/angerauchtete
-c 562 unit1exercise1/auswitztest
-c 311 unit1exercise1/ankatzeen
-
-
+m 775 unit1exercise1/angeraucher
+m 775 unit1exercise1/aussetzte
+m 775 unit1exercise1/angetrautest
+m 775 unit1exercise1/enschmeckse
+m 775 unit1exercise1/zerfahrer
+m 775 unit1exercise1/angekraust
+m 775 unit1exercise1/angehunds
+m 775 unit1exercise1/aufpflumheit
+m 775 unit1exercise1/aussetzte/gehundte
+m 775 unit1exercise1/angehunds/angesitzt
+m 775 unit1exercise1/aufpflumheit/ausgekaesen
+m 775 unit1exercise1/angekraust/geschmecken
+m 775 unit1exercise1/angekraust/enkatzetete
+m 363 unit1exercise1/angehunds/angesitzt/auspflums
+m 644 unit1exercise1/angehunds/angesitzt/aussetzt
+m 025 unit1exercise1/angehunds/angesitzt/angeschmecken
+m 255 unit1exercise1/angehunds/angesitzt/aufgerennen
+m 416 unit1exercise1/angehunds/angesitzt/getrauen
+m 270 unit1exercise1/aufpflumheit/ausgekaesen/eingehen
+m 145 unit1exercise1/angehunds/angesitzt/aufsetztete
+cs 616 unit1exercise1/aussetzte/gehundte
+cs 305 unit1exercise1/angehunds/angesitzt
+cs 700 unit1exercise1/aufpflumheit/ausgekaesen
+cs 537 unit1exercise1/angekraust/geschmecken
+cs 502 unit1exercise1/angekraust/enkatzetete
+c 536 unit1exercise1/angeraucher
+c 266 unit1exercise1/aussetzte
+c 771 unit1exercise1/angetrautest
+c 651 unit1exercise1/enschmeckse
+c 154 unit1exercise1/zerfahrer
+c 664 unit1exercise1/angekraust
+c 344 unit1exercise1/angehunds
+c 006 unit1exercise1/aufpflumheit
 sudo tar -cvzf unit1-solution1.tgz unit1exercise1
